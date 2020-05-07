3812753c78b67e8cf61a26a8303f5994166fb3f3
bet-better36 <61450126+bet-better36@users.noreply.github.com>
2020-05-07 21:50:15 +0900

    [5-9m]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.new
+  	@bookf = Book.find(params[:id])
+  end
+
+  def create
+  	@book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book)
+     else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+  	@book =  Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to '/books'
+    end
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+     @book.user_id = current_user.id
+  	if @book.update(book_params)
+  	flash[:notice] = "You have updated book successfully."
+  	redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	flash[:notice] = "You have deleted book successfully."
+  	redirect_to users_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body,)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'home#top'
+  get '/home/about' => 'home#about'
+
+  devise_for :users
+
+  resources :books, except: [:new]
+  resources :users, only: [:index, :show, :edit, :update, :destroy] 
+  	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
