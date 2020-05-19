ab252acbfbb3bc32ca189600f202f795a91cf958
bet-better36 <61450126+bet-better36@users.noreply.github.com>
2020-05-19 15:11:26 +0900

    [Add_favo,comment]

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :books, except: [:new]
+  resources :books, except: [:new] do
+  	resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create]
+   end
+   resources :book_comments, only: [:destroy]
   resources :users, only: [:index, :show, :edit, :update, :destroy] 
-  	
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
